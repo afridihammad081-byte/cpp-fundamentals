d049dabca0374546fe2516781f72844099a30292
afridihammad081-byte <afridihammad081@gmail.com>
2026-05-14 18:27:06 +0500

    Add files via upload

---
+++ fundemental.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+// hello world!
+{
+    cout<<"HELLOE WORLD"<<endl;
+    cout<<"Muhammaad Hammad Afridi"<<endl;
+        system("pause"); 
+    return 0;
+}
+
+
+
+// //write a c++ program to find the Rea of a rectangle and display it on the screen.
+ {
+        float L,W,Area,Parameter;
+                        cout<<"Hammad Afridi_051_Must_ajk";
+        cout<<"Enter The Lenghth ="<<endl<<"Enter the width ="<<endl;
+        cin>>L>>W;
+        Area=L*W;
+        cout<<"The Area of a Rectangle is = "<<Area<<endl;
+        Parameter=2*L + 2*W;
+        cout<<"Parameter of a Rectangle is = "<<Parameter<<endl;
+                system("pause");
+                return 0;
+      
+ }
+
+
+ // to check the number is completely divisiable by 2 and 5.
+ {
+    int num;
+            cout<<"Hammad Afridi-051_Must_ajk"<<endl;
+    cout<<"Enter the any  number = ";
+    cin>>num;
+    if(num % 2==0 && num % 5==0)
+    cout<<"The is completely divisiable by 2 and 5"<<endl;
+    else
+    cout<<"the number is not completely divisiabe by 2 and 5"<<endl;
+            system("pause");
+        return 0;
+
+   //write a c++ program to find the Rea of a rectangle and display it on the screen.
+   float L,W,Area,Parameter;
+                        cout<<"Hammad Afridi_051_Must_ajk";
+        cout<<"Enter The Lenghth ="<<endl<<"Enter the width ="<<endl;
+        cin>>L>>W;
+        Area=L*W;
+        cout<<"The Area of a Rectangle is = "<<Area<<endl;
+        Parameter=2*L + 2*W;
+        cout<<"Parameter of a Rectangle is = "<<Parameter<<endl;
+                system("pause");
+                return 0;
+  //              
+
+
+    //to find the root of a quadretic Formula.
+    int a,b,c,R1,R2;
+    cout<<"enter the value of a ="<<endl<<"enter the value of b ="<<endl<<"Enter the value of c ="<<endl;
+    cin>>a>>b>>c;
+    R1=(-b+sqrt(b*b-4*a*c)/2*a);
+    cout<<"Result 1 is = "<<R1<<endl;
+     R2=(-b-sqrt(b*b-4*a*c)/2*a);
+     cout<<"Result 2 is = "<<R2<<endl;
+                    system ("pause");
+    return 0;
+
+
+    // using assignment operators.
+
+
+    float value1, value2, sum, difference,division,Multiplication;
+    cout<<"Enter the value1"<<endl<<"enter the value2"<<endl;
+    cin>>value1>>value2;
+    sum=value1+value2;
+    difference=value1-value2;
+    division=value1/value2;
+    Multiplication=value1 * value2;
+    cout<<"The sum is = "<<sum<<endl;
+    cout<<"the difference = "<<difference<<endl;
+    cout<<"the division = "<<division<<endl;
+    cout<<"The Multiplication = "<<Multiplication<<endl;
+         system("pause");
+    return 0;
+
+            
+            
+ 
+
+
+
+
+
+
+
+
+
+
+// quadratic formlmula
+
+
+    float a,b,c,x1,x2;
+                    cout<<"Hammad Afridi_051_must_ajk"<<endl;
+    cout<<"enter the value of a , b, c, :";
+    cin>>a>>b>>c;
+    x1=-b+sqrt(b*b-4*a*c)/2*a;
+    x2=-b-sqrt(b*b-4*a*c)/2*a;
+    cout<<"Result 1 = "<<x1<<endl;    //if the result show nan this means this mean the result is imignary.
+    cout<<"Result 2 = "<<x2<<endl;
+    system ("pause");
+
+
+
+
+
+
+
+    return 0;
+}
